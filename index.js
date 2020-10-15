aef2f9e37698af97d64c4b99c1b58ffcd7ffbdaf
Yuyeol <uy23@naver.com>
2020-10-15 19:11:32 +0900

    done

---
+++ index.js
@@ -0,0 +1,38 @@
+const numberBtn = document.querySelectorAll(".js-number");
+const operationBtn = document.querySelectorAll(".js-operation");
+const outputBlock = document.querySelector(".js-output");
+let numClicked = true;
+let operClicked = false;
+
+const handleClickNum = (event) => {
+  if (numClicked == true) {
+    const { id: number } = event.path[0];
+    outputBlock.innerText = outputBlock.innerText + number;
+    operClicked = true;
+  }
+};
+
+const handleClickOper = (event) => {
+  const { id: oper } = event.path[0];
+
+  if (oper == "C") {
+    outputBlock.innerText = "";
+  } else if (oper == "=") {
+    outputBlock.innerText = eval(outputBlock.innerText);
+    operClicked = true;
+    numClicked = false;
+  } else {
+    if (operClicked == true) {
+      outputBlock.innerText = outputBlock.innerText + oper;
+      numClicked = true;
+      operClicked = false;
+    }
+  }
+};
+
+for (i = 0; i < numberBtn.length; i++) {
+  numberBtn[i].addEventListener("click", handleClickNum);
+}
+for (i = 0; i < operationBtn.length; i++) {
+  operationBtn[i].addEventListener("click", handleClickOper);
+}
